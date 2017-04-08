1e3975b848cafe90a02b5eec5808e3e2772d1332
BreZzy33 <kenni1@live.ru>
2017-04-08 22:32:26 +0700

    Interface without function.

--- Bilinear-Interpolation-New/Bilinear-Interpolation-New/Form1.Designer.cs
+++ Bilinear-Interpolation-New/Bilinear-Interpolation-New/Form1.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.photoBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resizeBtn = new System.Windows.Forms.Button();
+            this.resizeBox = new System.Windows.Forms.TextBox();
+            this.imageBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.photoBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // photoBox
+            // 
+            this.photoBox.Location = new System.Drawing.Point(12, 12);
+            this.photoBox.Name = "photoBox";
+            this.photoBox.Size = new System.Drawing.Size(804, 585);
+            this.photoBox.TabIndex = 0;
+            this.photoBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(822, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Масштаб в % :";
+            // 
+            // resizeBtn
+            // 
+            this.resizeBtn.Location = new System.Drawing.Point(825, 278);
+            this.resizeBtn.Name = "resizeBtn";
+            this.resizeBtn.Size = new System.Drawing.Size(149, 34);
+            this.resizeBtn.TabIndex = 2;
+            this.resizeBtn.Text = "Изменить размер";
+            this.resizeBtn.UseVisualStyleBackColor = true;
+            this.resizeBtn.Click += new System.EventHandler(this.resizeBtn_Click);
+            // 
+            // resizeBox
+            // 
+            this.resizeBox.Location = new System.Drawing.Point(825, 252);
+            this.resizeBox.Name = "resizeBox";
+            this.resizeBox.Size = new System.Drawing.Size(100, 20);
+            this.resizeBox.TabIndex = 3;
+            this.resizeBox.TextChanged += new System.EventHandler(this.resizeBox_TextChanged);
+            this.resizeBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.resizeBox_KeyPress);
+            // 
+            // imageBtn
+            // 
+            this.imageBtn.Location = new System.Drawing.Point(822, 37);
+            this.imageBtn.Name = "imageBtn";
+            this.imageBtn.Size = new System.Drawing.Size(149, 34);
+            this.imageBtn.TabIndex = 2;
+            this.imageBtn.Text = " Загрузить изображение";
+            this.imageBtn.UseVisualStyleBackColor = true;
+            this.imageBtn.Click += new System.EventHandler(this.imageBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(825, 535);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(149, 34);
+            this.exitBtn.TabIndex = 2;
+            this.exitBtn.Text = "Выйти";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(986, 609);
+            this.Controls.Add(this.resizeBox);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.imageBtn);
+            this.Controls.Add(this.resizeBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.photoBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bilinear Interpolation Main";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.photoBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox photoBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button resizeBtn;
+        private System.Windows.Forms.TextBox resizeBox;
+        private System.Windows.Forms.Button imageBtn;
+        private System.Windows.Forms.Button exitBtn;
     }
 }
 
--- Bilinear-Interpolation-New/Bilinear-Interpolation-New/Form1.cs
+++ Bilinear-Interpolation-New/Bilinear-Interpolation-New/Form1.cs
@@ -15,6 +15,64 @@ namespace Bilinear_Interpolation_New
         public Form1()
         {
             InitializeComponent();
+            resizeBox.KeyPress += new KeyPressEventHandler(resizeBox_KeyPress);
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void imageBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog imageDialog = new OpenFileDialog();
+            imageDialog.Title = "Choose pictures!";
+            imageDialog.Filter = "Image files (*.bmp, *.jpeg, *.jpg, *.png)| *.bmp; *.jpeg; *.jpg; *.png |All Files| *.*";
+            imageDialog.FilterIndex = 1;
+            
+            if (imageDialog.ShowDialog() == DialogResult.OK)
+            {
+                photoBox.ImageLocation = imageDialog.FileName;
+                photoBox.Load();
+                if(resizeBox.Enabled==false)
+                { resizeBox.Enabled = true; }
+            }
+        }
+
+        private void resizeBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(resizeBox.Text))
+            {
+                resizeBox.Enabled = true;
+            }
+            else
+            {
+                resizeBox.Enabled = false;
+            }
+        }
+
+        private void resizeBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsNumber(e.KeyChar) || e.KeyChar==(char)8)
+            {
+                return;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resizeBox.Enabled = false;
+            resizeBtn.Enabled = false;
+        }
+
+        private void resizeBtn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
